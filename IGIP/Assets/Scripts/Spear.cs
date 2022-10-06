4ee85cc85502b85b61382dc4b29220d466ee2dfc
anminwoo <minwoo5995@gmail.com>
2022-10-06 19:30:41 +0900

    Add Script
    Spear

---
+++ IGIP/Assets/Scripts/Spear.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        transform.Translate(Vector2.down * (speed * Time.deltaTime));
+    }
+}
