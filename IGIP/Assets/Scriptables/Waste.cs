24ba6a7e8534e8a6a13a823772763bd6e0f0c3af
anminwoo <minwoo5995@gmail.com>
2022-10-08 17:12:49 +0900

    Add Scriptables

---
+++ IGIP/Assets/Scriptables/Waste.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Waste", menuName = "Waste")]
+public class Waste : ScriptableObject
+{
+    public string wasteName;
+    public int score;
+}
+ 
