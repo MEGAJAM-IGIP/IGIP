54b0bab4f58309fccd84dddddbb69ac26192eed1
anminwoo <minwoo5995@gmail.com>
2022-10-09 15:42:19 +0900

    Add Script
    Life

---
+++ IGIP/Assets/Scripts/Life.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Scriptables;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField] private LifeData lifeData;
+    private Sprite curSprite;
+    public int curHealth;
+    public void Start()
+    {
+        curSprite = lifeData.liveSprite;
+        curHealth = lifeData.health;
+    }
+
+    public void Die()
+    {
+        curSprite = lifeData.deadSprite;
+        // 물고기 위로 뜨는 거 구현
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Waste"))
+        {
+            curHealth--;
+            if (curHealth == 0)
+            {
+                Die();
+            }
+        }
+    }
+}
