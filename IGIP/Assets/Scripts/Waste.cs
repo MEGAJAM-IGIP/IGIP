83402f9990254daaf0a190374644c9cbc61c3d01
anminwoo <minwoo5995@gmail.com>
2022-10-09 15:39:41 +0900

    Move Script
    Waste

---
+++ IGIP/Assets/Scripts/Waste.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Waste : MonoBehaviour
+{
+    public WasteData wasteData;
+    public float curSpeed;
+    public Vector2 moveDir;
+
+    private void Start()
+    {
+        curSpeed = Random.Range(wasteData.minSpeed, wasteData.maxSpeed);
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveDir * (curSpeed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Spear"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this);
+    }
+}
