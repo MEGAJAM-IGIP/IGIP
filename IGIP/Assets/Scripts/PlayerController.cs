94a63e7e26f8022eeeb54572a3180bec6df72a5c
anminwoo <minwoo5995@gmail.com>
2022-10-09 15:40:13 +0900

    Add function
    LimitPlayerPos

--- IGIP/Assets/Scripts/PlayerController.cs
+++ IGIP/Assets/Scripts/PlayerController.cs
@@ -19,7 +19,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        LimitPlayerPos();
+        Move();
+    }
+
+    public void Move()
+    {
         Vector2 movePos = new Vector2(Input.GetAxisRaw("Horizontal"), 0).normalized;
         rigid.velocity = movePos * speed;
+    }
+
+    public void LimitPlayerPos()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < 0f) pos.x = 0f;
+        if (pos.x > 1f) pos.x = 1f;
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
