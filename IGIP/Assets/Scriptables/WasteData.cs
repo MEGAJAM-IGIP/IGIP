7fb22ca09eb4142d855377c88c6559a7b82e1837
anminwoo <minwoo5995@gmail.com>
2022-10-09 20:26:37 +0900

    Modify script

--- IGIP/Assets/Scriptables/WasteData.cs
+++ IGIP/Assets/Scriptables/WasteData.cs
@@ -5,17 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "new Waste", menuName = "Waste")]
 public class WasteData : ScriptableObject
 {
-    public enum WasteType
-    {
-        Plastic,
-        Paper,
-        Styrofoam,
-        Rubber,
-        Cigarette,
-        Glass
-    };
     public string wasteName;
-    public WasteType wasteType;
+    public AudioClip wasteSound;
+    public int score;
 
     [Header("waste move Speed")]
     public float minSpeed;
