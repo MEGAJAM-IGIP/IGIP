eb9b8109c7a64f92d49026caccdc0828091347a4
anminwoo <minwoo5995@gmail.com>
2022-10-10 14:22:04 +0900

    Modify script

--- IGIP/Assets/Scripts/SceneSystem.cs
+++ IGIP/Assets/Scripts/SceneSystem.cs
@@ -5,44 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneSystem : MonoBehaviour
 {
-    private static SceneSystem instance;
-
-    public static SceneSystem Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                var obj = FindObjectOfType<SceneSystem>();
-
-                if (obj != null)
-                {
-                    instance = obj;
-                }
-                else
-                {
-                    var newObj = new GameObject().AddComponent<SceneSystem>();
-                    instance = newObj;
-                    newObj.name = "SceneSystem";
-                }
-            }
-
-            return instance;
-        }
-    }
-
-    public bool isGameOver;
-
     private void Awake()
     {
-        var objs = FindObjectsOfType<SceneSystem>();
-        if (objs.Length != 1)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        
-        DontDestroyOnLoad(gameObject);
     }
     
     public void SceneChange(int sceneNumber)
